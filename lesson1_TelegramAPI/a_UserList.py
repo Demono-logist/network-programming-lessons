1c5ecedfbe028727696b693ce93617619395c5ea
Demono-logist <nikita.n.student@gmail.com>
2025-01-02 02:22:48 +0200

    Add files via upload

---
+++ lesson1_TelegramAPI/a_UserList.py
@@ -0,0 +1,40 @@
+﻿from telethon.sync import TelegramClient
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import ChannelParticipantsSearch
+
+# Завантажуємо змінні з .env файлу
+load_dotenv('config.env')
+
+# Дані Telegram API
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+
+# Посилання на чат
+chat_public_link = os.getenv('CHAT_PUBLIC_USERNAME')
+
+with TelegramClient('session_name', api_id, api_hash) as client:
+    # Отримання об'єкта чату
+    chat_entity = client.get_entity(chat_public_link)
+
+    # Ініціалізація параметрів
+    all_participants = []
+    offset = 0
+    limit = 150
+
+    # Отримання списку учасників
+    while True:
+        participants = client(GetParticipantsRequest(
+            channel=chat_entity,
+            filter=ChannelParticipantsSearch(''),
+            offset=offset,
+            limit=limit,
+            hash=0  # Встановлюємо hash як 0
+        ))
+        all_participants.extend(participants.users)
+        if not participants.users:
+            break
+        offset += len(participants.users)
+
+    # Вивід списку учасників
+    for participant in all_participants:
+        print(f"ID: {participant.id}, Username: {participant.username}, Name: {participant.first_name}")
---
+++ lesson1_TelegramAPI/b_Send_Messages_to_the_User_or_Chat.py
@@ -0,0 +1,37 @@
+﻿from telethon import TelegramClient
+from dotenv import load_dotenv
+import os
+
+# Завантажуємо змінні з .env файлу
+load_dotenv('config.env')
+
+# Зчитуємо API ID та API Hash з .env файлу
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+
+# Зчитуємо chat_username та message з .env файлу
+chat_username = os.getenv('USERNAME_OR_LINK')
+message = os.getenv('MESSAGE')
+
+# Налаштування клієнта
+client = TelegramClient('session_name', api_id, api_hash)
+
+# Функція для відправки повідомлення
+async def send_message():
+    try:
+        # Підключення до Telegram
+        await client.start()
+
+        # Відправка повідомлення
+        await client.send_message(chat_username, message)
+
+        print(f"Повідомлення надіслано до {chat_username}!")
+
+    except Exception as e:
+        print(f"Сталася помилка: {e}")
+    finally:
+        await client.disconnect()
+
+# Викликаємо функцію для відправки повідомлення
+with client:
+    client.loop.run_until_complete(send_message())
